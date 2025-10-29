c382235db902ace29e4285a0b2acdf2458a7d0f6
Julio Faerman <julio@faermanj.com>
2025-10-29 07:53:55 +0100

    Improved logging

--- src/0prelude.sh
+++ src/0prelude.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Utility functions for bash scripts
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%N}}")" >/dev/null 2>&1 && pwd)"
 
 # Define colors
 readonly RED="\033[0;31m"
@@ -8,28 +8,43 @@ readonly GREEN="\033[0;32m"
 readonly YELLOW="\033[0;33m"
 readonly BLUE="\033[0;34m"
 readonly NO_COLOR="\033[0m"
+readonly MAGENTA="\033[0;35m"
 
 # Generic log function
+LOG_FORMAT="{color}{timestamp} [{level}] {message}{nocolor}"
+
 log() {
     local level="$1"; shift
     local message="$1"; shift
     local color="$NO_COLOR"
+    local timestamp
+    timestamp="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+
+    # Trim spaces from log level
+    levelmatch="$(echo "$level" | xargs)"
 
     # Determine color based on log level
-    case "$level" in
+    case "$levelmatch" in
         INFO) color="$GREEN" ;;
         DEBUG) color="$BLUE" ;;
         WARN) color="$YELLOW" ;;
         ERROR) color="$RED" ;;
+        TRACE) color="$MAGENTA" ;;
     esac
 
-    # Print the message in the determined color
-    echo -e "${color}[${level}]${NO_COLOR} $message" >&2
+        # Use user-definable format
+        local formatted="$LOG_FORMAT"
+            formatted="${formatted//\{timestamp\}/$timestamp}"
+            formatted="${formatted//\{color\}/$color}"
+            formatted="${formatted//\{level\}/$level}"
+            formatted="${formatted//\{nocolor\}/$NO_COLOR}"
+            formatted="${formatted//\{message\}/$message}"
+        echo -e "$formatted" >&2
 }
 
 # Specific log level functions
 info() {
-    log "INFO" "$@"
+    log 'INFO ' "$@"
 }
 
 debug() {
@@ -37,9 +52,13 @@ debug() {
 }
 
 warn() {
-    log "WARN" "$@"
+    log 'WARN ' "$@"
 }
 
 error() {
     log "ERROR" "$@"
+}
+
+trace() {
+    log "TRACE" "$@"
 }
---
+++ test.sh
@@ -0,0 +1,8 @@
+source ./bin/utils.sh
+
+info "This is an info message."
+debug "This is a debug message."
+warn "This is a warning message."
+error "This is an error message."
+trace "This is a trace message."
+
